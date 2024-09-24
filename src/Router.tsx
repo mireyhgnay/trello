5871f95af9f2128bf69505d226557bfc2edc724a
hyerim <hyerimiya1216@gmail.com>
2024-09-24 13:47:06 +0900

    Feat : Trello 초기 셋팅 최종 완료

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,34 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
-import Coins from './routes/Coins';
-import Coin from './routes/Coin';
+
 import App from './App';
-// import Price from './routes/Price';
-import Chart from './routes/Chart';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    children: [
-      {
-        path: '',
-        element: <Coins />,
-      },
-      {
-        path: '/:coinId',
-        element: <Coin />,
-        children: [
-          // {
-          //   path: '/:coinId/price',
-          //   element: <Price />,
-          // },
-          {
-            path: '/:coinId/chart',
-            element: <Chart />,
-          },
-        ],
-      },
-    ],
   },
 ]);
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 
 import router from './Router';
 import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme } from './theme';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +13,10 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RecoilRoot>
+      <ThemeProvider theme={darkTheme}>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </RecoilRoot>
   </React.StrictMode>
 );
