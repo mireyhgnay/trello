9aad0ad95d7abecbf2b29a11dc2453c994ffd1ad
hyerim <hyerimiya1216@gmail.com>
2024-09-28 20:59:38 +0900

    Feat: Cross Board Movement

--- src/App.tsx
+++ src/App.tsx
@@ -25,17 +25,34 @@ export default function App() {
   const [toDos, setToDos] = useRecoilState(toDoState);
 
   const onDragEnd = (info: DropResult) => {
-    console.log(info);
     const { destination, draggableId, source } = info;
+
+    if (!destination) return;
+
+    // same board movement
     if (destination?.droppableId === source.droppableId) {
-      // same board movement
       setToDos((allBoards) => {
-        const boardCopy = [...allBoards[source.droppableId]];
-        boardCopy.splice(source.index, 1);
-        boardCopy.splice(destination?.index, 0, draggableId);
+        const boardCopy = [...allBoards[source.droppableId]]; // 선택한 아이템의 보드 전체 배열 복사 (Doing에 있는 요소를 선택했다면 Doing전체 배열을 복사한다.)
+        boardCopy.splice(source.index, 1); // 선택한 요소 제거
+        boardCopy.splice(destination?.index, 0, draggableId); // 옮긴 index에 선택한 요소 추가
         return {
           ...allBoards,
-          [source.droppableId]: boardCopy,
+          [source.droppableId]: boardCopy, // 변형한 배열을 반환
+        };
+      });
+    }
+
+    // cross board movement
+    if (destination.droppableId !== source.droppableId) {
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]]; // 선택한 요소의 보드의 배열
+        const destinationBoard = [...allBoards[destination.droppableId]]; // 옮겨 도착한 보드의 배열
+        sourceBoard.splice(source.index, 1); // 기존 보드에서는 제거
+        destinationBoard.splice(destination?.index, 0, draggableId); // 옮긴 보드에는 요소 추가
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
         };
       });
     }
